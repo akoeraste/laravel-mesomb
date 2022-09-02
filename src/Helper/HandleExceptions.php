0e61aa1bbdc7ca4060fe6f57f1b498d5cb71a941
Daniel De Vigny <hachther@gmail.com>
2022-09-02 07:20:35 +0100

    Add ApplicationNotFoundException

--- src/Helper/HandleExceptions.php
+++ src/Helper/HandleExceptions.php
@@ -3,28 +3,35 @@
 namespace Hachther\MeSomb\Helper;
 
 use Illuminate\Http\Client\Response;
-use Hachther\MeSomb\Exceptions\{InsufficientBalanceException, InvalidAmountException, InvalidPhoneNumberException, InvalidPinException, TimeoutException};
+use Hachther\MeSomb\Exceptions\{InsufficientBalanceException,
+    InvalidAmountException,
+    InvalidPhoneNumberException,
+    InvalidPinException,
+    TimeoutException,
+    ApplicationNotFoundException
+};
 
 trait HandleExceptions
 {
     public $errorCodes = [
         'subscriber-insufficient-balance' => InsufficientBalanceException::class,
-        'subscriber-not-found'            => InvalidPhoneNumberException::class,
-        'subscriber-invalid-length'       => InvalidPhoneNumberException::class,
-        'subscriber-invalid-secret-code'  => InvalidPinException::class,
-        'subscriber-invalid-min-amount'   => InvalidAmountException::class,
-        'subscriber-invalid-max-amount'   => InvalidAmountException::class,
-        'subscriber-timeout'              => TimeoutException::class,
-        'subscriber-internal-error'       => TimeoutException::class,
+        'application-not-found' => ApplicationNotFoundException::class,
+        'subscriber-not-found' => InvalidPhoneNumberException::class,
+        'subscriber-invalid-length' => InvalidPhoneNumberException::class,
+        'subscriber-invalid-secret-code' => InvalidPinException::class,
+        'subscriber-invalid-min-amount' => InvalidAmountException::class,
+        'subscriber-invalid-max-amount' => InvalidAmountException::class,
+        'subscriber-timeout' => TimeoutException::class,
+        'subscriber-internal-error' => TimeoutException::class,
     ];
 
     public function handleException(Response $response)
     {
-        if (! config('mesomb.throw_exceptions')) {
+        if (!config('mesomb.throw_exceptions')) {
             return;
         }
 
-        $body = (object) $response->json();
+        $body = (object)$response->json();
 
         if (isset($this->errorCodes[$body->code])) {
             $class = $this->errorCodes[$body->code];
